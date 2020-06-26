158021670a3955074d112ce1ff6c1fe0bdc8f605
shani <56839113+shani1610@users.noreply.github.com>
2020-06-26 19:10:21 +0300

    working on sintra and adding RANSAC

--- code/my_homography_shani.py
+++ code/my_homography_shani.py
@@ -76,29 +76,46 @@ def Translation(im1, H):
     return H_trans, out_size, axis_arr
 
 
-def getScaled(im2, warp_im1, axis_arr):
+def getScaled(im2, warp_im1, axis_arr, warp_is_left):
     #
     axis_y_top = axis_arr[0]
     axis_y_bottom = axis_arr[1]
     axis_x_left = axis_arr[2]
     axis_x_right = axis_arr[3]
+    print('axis_y_top')
+    print(axis_y_top)
+    print('axis_y_bottom')
+    print(axis_y_bottom)
+    print('axis_x_left')
+    print(axis_x_left)
+    print('axis_x_right')
+    print(axis_x_right)
 
     shape_y = max(axis_y_bottom, im2.shape[0]) - min(axis_y_top, 0)
     shape_x = max(axis_x_right, im2.shape[1]) - min(axis_x_left, 0)
+    print('shape_y')
+    print(shape_y)
+    print('shape_x')
+    print(shape_x)
 
     warp_im1_scaled = np.zeros((shape_y, shape_x, 3))
     im2_scaled = np.zeros(warp_im1_scaled.shape)
-    #
     im2_mask = np.where(im2 > 0)
-    im2_scaled[im2_mask[0] - axis_y_top, im2_mask[1] - axis_x_left, im2_mask[2]] = im2[im2_mask]
-    # print('im2_scaled')
-    # print(im2_scaled)
-    #
+    if warp_is_left:
+        im2_scaled[im2_mask[0] - min(axis_y_top, 0), im2_mask[1] - axis_x_left, im2_mask[2]] = im2[im2_mask]
+    else:
+        im2_scaled[im2_mask[0] - min(axis_y_top, 0), im2_mask[1], im2_mask[2]] = im2[im2_mask]
+    plt.figure(2)
+    plt.imshow(im2_scaled)
+    plt.show()
     im1_warp_mask = np.where(warp_im1 > 0)
-    warp_im1_scaled[im1_warp_mask] = warp_im1[im1_warp_mask]
-    # print('warp_im1_scaled')
-    # print(warp_im1_scaled)
-    #
+    if warp_is_left:
+        warp_im1_scaled[im1_warp_mask] = warp_im1[im1_warp_mask]
+    else:
+        warp_im1_scaled[im1_warp_mask[0], im1_warp_mask[1] + axis_x_left, im1_warp_mask[2]] = warp_im1[im1_warp_mask]
+    plt.figure(3)
+    plt.imshow(warp_im1_scaled)
+    plt.show()
     return warp_im1_scaled, im2_scaled
 
     # Extra functions end
@@ -260,17 +277,103 @@ def prepareToMerge(xLeft, xRight, yTop, yBootom, warp_im1, im2):
     return warp_im1_big, im2_big
 
 
-def panoramaTwoImg(im1, im2):
+def panoramaTwoImg(im1, im2, warp_is_left):
     p2, p1 = getPoints_SIFT(im1, im2)
-    H2to1 = computeH(p1, p2)
-    H_trans, out_size, axis_arr = Translation(im1, H2to1)
+    #H2to1 = computeH(p1, p2)
+    nIter = 1000
+    tol = 5
+    bestH = ransacH(p1, p2, nIter, tol)
+    H_trans, out_size, axis_arr = Translation(im1, bestH)
+    print('axis arr')
+    print(axis_arr)
     warp_im1 = warpH(im1, H_trans, out_size)
-    warp_im1_scaled, im2_scaled = getScaled(im2, warp_im1, axis_arr)
+    plt.figure(4)
+    plt.imshow(warp_im1)
+    plt.show()
+    warp_im1_scaled, im2_scaled = getScaled(im2, warp_im1, axis_arr, warp_is_left)
+    panoramaTest = imageStitching(im2_scaled, warp_im1_scaled)
+    plt.figure(5)
+    plt.imshow(panoramaTest)
+    plt.show()
+    return panoramaTest, H2to1
+
+def panoramaTwoImgTmp(im1, im2, warp_is_left, H2to1):
+    p1, p2 = getPoints_SIFT(im1, im2)
+    #H2to1 = computeH(p1, p2)
+    nIter = 1000
+    tol = 5
+    bestH = ransacH(p1, p2, nIter, tol)
+    # H3to2 = np.array([[-5.11736316e-03,  3.64884082e-04,  7.42714231e-01],
+    #                   [-1.72835740e-03, -4.79612570e-03, 6.69555992e-01],
+    #                   [-1.81873113e-06,  3.28148995e-08, -4.24677187e-03]])
+    # H = H3to2@H2to1
+    H_trans, out_size, axis_arr = Translation(im1, bestH)
+    print('axis arr')
+    print(axis_arr)
+    warp_im1 = warpH(im1, H_trans, out_size)
+    plt.figure(4)
+    plt.imshow(warp_im1)
+    plt.show()
+    warp_im1_scaled, im2_scaled = getScaled(im2, warp_im1, axis_arr, warp_is_left)
     panoramaTest = imageStitching(im2_scaled, warp_im1_scaled)
     plt.figure(5)
     plt.imshow(panoramaTest)
     plt.show()
     return panoramaTest
+
+def ransacH_Someone(p1, p2, nIter, tol=5):
+    bestH = np.zeros(3)
+    max_score = 0
+    for i in range(nIter):
+        model = np.random.choice(p1.shape[0], 4)  # choose 4 random matches
+
+        H = computeH(p1[model].T, p2[model].T)  # compute H for model
+        p1_match = H @ np.vstack((p2.T, np.ones(1, p2.shape[0]))) # transform to homogeneous
+        p1_match /= p1_match[2, :]  # normalization
+        p1_match = p1_match[:2, :].T  # convert back to (x,y) points
+        dist = np.linalg.norm(p1-p1_match, axis=1)
+        score = np.sum(dist <= tol)
+        if max_score < score:
+            max_score = score
+            bestH = H
+    return bestH
+
+
+def ransacH(p1, p2, nIter, tol):
+    """
+    Your code here  # TODO: DOC!
+    """
+    matches_Number = p1.shape[1]
+    minSizeToCalcH = 4  # need 4 couples to calc H linearly
+    usedGroupIndices = []
+    bestScore = 0
+    bestInliersIndices = np.array([])
+    for _ in range(nIter):
+        groupIndices = np.random.choice(matches_Number, minSizeToCalcH)
+        while groupIndices in usedGroupIndices:
+            # if we already used those matches, take others
+            groupIndices = np.random.choice(matches_Number, minSizeToCalcH)
+        usedGroupIndices.append(groupIndices)
+        p1_sample = p1[:, groupIndices]  # sample randomly
+        p2_sample = p2[:, groupIndices]  # sample randomly
+        currH = computeH(p1_sample, p2_sample)  # calc H
+
+        # count inliers:
+        p2_with_ones = np.concatenate((p2, np.ones((1, matches_Number))), axis=0)
+        p1_est = currH @ p2_with_ones
+        p1_est /= p1_est[2, :]
+        p1_est = p1_est[:2, :]
+        inliesIndices = np.where(((p1_est - p1) ** 2).sum(1) < tol)[0]
+
+        # calc score and if it's better update parameters
+        currScore = inliesIndices.size / matches_Number
+        if currScore > bestScore:
+            bestScore = currScore
+            # bestH = currH
+            bestInliersIndices = inliesIndices
+
+    bestH = computeH(p1[:, bestInliersIndices], p2[:, bestInliersIndices])
+    return bestH
 
 def beachTest():
     # images beach
@@ -318,55 +421,93 @@ def beachTest():
     cv2.imwrite('./my_data/beach_panorama_final_beach_SIFT.jpg', panorama_final_beach)
     return beachPanoramaTest
 
+def sintraTest():
+    downSampleRate = 4
+
+    sintra1 = cv2.imread('data/sintra1.JPG')
+    sintra2 = cv2.imread('data/sintra2.JPG')
+    sintra3 = cv2.imread('data/sintra3.JPG')
+    sintra4 = cv2.imread('data/sintra4.JPG')
+    sintra5 = cv2.imread('data/sintra5.JPG')
+
+    im_sintra1 = cv2.cvtColor(sintra1, cv2.COLOR_BGR2RGB)
+    im_sintra2 = cv2.cvtColor(sintra2, cv2.COLOR_BGR2RGB)
+    im_sintra3 = cv2.cvtColor(sintra3, cv2.COLOR_BGR2RGB)
+    im_sintra4 = cv2.cvtColor(sintra4, cv2.COLOR_BGR2RGB)
+    im_sintra5 = cv2.cvtColor(sintra5, cv2.COLOR_BGR2RGB)
+
+    im_sintra1 = cv2.resize(im_sintra1, (im_sintra1.shape[0] // downSampleRate,
+                                         im_sintra1.shape[1] // downSampleRate))
+    im_sintra2 = cv2.resize(im_sintra2, (im_sintra2.shape[0] // downSampleRate,
+                                        im_sintra2.shape[1] // downSampleRate))
+    im_sintra3 = cv2.resize(im_sintra3, (im_sintra3.shape[0] // downSampleRate,
+                                         im_sintra3.shape[1] // downSampleRate))
+    im_sintra4 = cv2.resize(im_sintra4, (im_sintra4.shape[0] // downSampleRate,
+                                         im_sintra4.shape[1] // downSampleRate))
+    im_sintra5 = cv2.resize(im_sintra5, (im_sintra5.shape[0] // downSampleRate,
+                                         im_sintra5.shape[1] // downSampleRate))
+
+    # 2+3
+    panorama23, H3to2 = panoramaTwoImg(im_sintra2, im_sintra3,  warp_is_left=False)
+    cv2.imwrite('./my_data/sintra_panorama23_SIFT.jpg', panorama23)
+
+    # 3+4
+    panorama34, H3to4 = panoramaTwoImg(im_sintra4, im_sintra3,  warp_is_left=True)
+    cv2.imwrite('./my_data/sintra_panorama34_SIFT.jpg', panorama34)
+
+    # 1+2
+    panorama12, H2to1 = panoramaTwoImg(im_sintra1, im_sintra2,  warp_is_left=False)
+    cv2.imwrite('./my_data/sintra_panorama12_SIFT.jpg', panorama12)
+
+    # 4+5
+    panorama45, H4to5 = panoramaTwoImg(im_sintra5, im_sintra4,  warp_is_left=True)
+    cv2.imwrite('./my_data/sintra_panorama45_SIFT.jpg', panorama45)
+
+    # 2+3+4
+    panorama23 = cv2.imread('./my_data/sintra_panorama23_SIFT.jpg')
+    panorama23 = cv2.cvtColor(panorama23, cv2.COLOR_BGR2RGB)
+    panorama34 = cv2.imread('./my_data/sintra_panorama34_SIFT.jpg')
+    panorama34 = cv2.cvtColor(panorama34, cv2.COLOR_BGR2RGB)
+    panorama234, H23to34 = panoramaTwoImg(panorama34, panorama23, warp_is_left=True)
+    cv2.imwrite('./my_data/sintra_panorama234_SIFT.jpg', panorama234)
+
+    # 1+2+3+4
+    panorama234 = cv2.imread('./my_data/sintra_panorama234_SIFT.jpg')
+    panorama234 = cv2.cvtColor(panorama234, cv2.COLOR_BGR2RGB)
+    panorama1234 = panoramaTwoImgTmp(im_sintra1, panorama234, False, H2to1)
+    cv2.imwrite('./my_data/sintra_panorama1234_SIFT.jpg', panorama1234)
+
+    #1+2+3+4+5
+    panorama1234 = cv2.imread('./my_data/sintra_panorama1234_SIFT.jpg')
+    panorama1234 = cv2.cvtColor(panorama1234, cv2.COLOR_BGR2RGB)
+    panorama_final_sintra, Hfinal = panoramaTwoImgTmp(im_sintra5, panorama1234, True, H4to5)
+    cv2.imwrite('./my_data/sintra_panorama_final_SIFT.jpg', panorama_final_sintra)
+    return panorama_final_sintra
 
 if __name__ == '__main__':
         print('my_homography')
-        downSampleRate = 4
-        image1 = cv2.imread('data/incline_L.png')
-        image2 = cv2.imread('data/incline_R.png')
-        im1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
-        im2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-        #im1 = im1[::downSampleRate, ::downSampleRate, :]  # downSample because the memory problems
-        #im2 = im2[::downSampleRate, ::downSampleRate, :]  # downSample because the memory problems
+        # downSampleRate = 4
+        # image1 = cv2.imread('data/incline_L.png')
+        # image2 = cv2.imread('data/incline_R.png')
+        # im1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+        # im2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+        # #im1 = im1[::downSampleRate, ::downSampleRate, :]  # downSample because the memory problems
+        # #im2 = im2[::downSampleRate, ::downSampleRate, :]  # downSample because the memory problems
+        # panoramaTest = panoramaTwoImg(im2, im1, warp_is_left=False)
 
-        # images beach
-        beach1 = cv2.imread('data/sintra5.JPG')
-        beach2 = cv2.imread('data/sintra4.JPG')
-        beach3 = cv2.imread('data/sintra3.JPG')
-        beach4 = cv2.imread('data/sintra2.JPG')
-        beach5 = cv2.imread('data/sintra1.JPG')
-        im_beach1 = cv2.cvtColor(beach1, cv2.COLOR_BGR2RGB)
-        im_beach2 = cv2.cvtColor(beach2, cv2.COLOR_BGR2RGB)
-        im_beach3 = cv2.cvtColor(beach3, cv2.COLOR_BGR2RGB)
-        im_beach4 = cv2.cvtColor(beach4, cv2.COLOR_BGR2RGB)
-        im_beach5 = cv2.cvtColor(beach5, cv2.COLOR_BGR2RGB)
+        print('my sintra')
+        panorama_final_sintra = sintraTest()
+        plt.figure(6)
+        plt.imshow(panorama_final_sintra)
+        plt.show()
 
-        # 1+2
-        im_beach1 = cv2.resize(im_beach1, (im_beach1.shape[0] // downSampleRate,
-                                               im_beach1.shape[1] // downSampleRate))
-        im_beach2 = cv2.resize(im_beach2, (im_beach2.shape[0] // downSampleRate,
-                                               im_beach2.shape[1] // downSampleRate))
-        panorama12 = panoramaTwoImg(im_beach1, im_beach2)
-        cv2.imwrite('./my_data/beach_panorama12_SIFT.jpg', panorama12)
-
-        # 1+2+3
-        panorama12 = cv2.imread('./my_data/beach_panorama12_SIFT.jpg')
-        panorama12 = cv2.cvtColor(panorama12, cv2.COLOR_BGR2RGB)
-        im_beach3 = cv2.resize(im_beach3, (im_beach3.shape[0] // downSampleRate,
-                                               im_beach3.shape[1] // downSampleRate))
-        panorama123 = panoramaTwoImg(panorama12, im_beach3)
-        cv2.imwrite('./my_data/beach_panorama123_SIFT.jpg', panorama123)
-
-        # 1+2+3+4
-        im_beach4 = cv2.resize(im_beach4, (im_beach4.shape[0] // downSampleRate,
-                                               im_beach4.shape[1] // downSampleRate))
-        panorama1234 = panoramaTwoImg(panorama123, im_beach4)
-        cv2.imwrite('./my_data/beach_panorama1234_SIFT.jpg', panorama1234)
-
-        # 1+2+3+4+5
-        panorama1234 = cv2.imread('./my_data/beach_panorama1234_SIFT.jpg')
-        panorama1234 = cv2.cvtColor(panorama1234, cv2.COLOR_BGR2RGB)
-        im_beach5 = cv2.resize(im_beach5, (im_beach5.shape[0] // downSampleRate,
-                                               im_beach5.shape[1] // downSampleRate))
-        panorama_final_beach = panoramaTwoImg(panorama1234, im_beach5)
-        cv2.imwrite('./my_data/beach_panorama_final_beach_SIFT.jpg', panorama_final_beach)
+        # downSampleRate = 4
+        # sintra1 = cv2.imread('data/sintra1.JPG')
+        # im_sintra1 = cv2.cvtColor(sintra1, cv2.COLOR_BGR2RGB)
+        # panorama234 = cv2.imread('./my_data/sintra_panorama234_SIFT.jpg')
+        # panorama234 = cv2.cvtColor(panorama234, cv2.COLOR_BGR2RGB)
+        # panorama1234, Hsmt1 = panoramaTwoImg(im_sintra1, panorama234, warp_is_left=False)
+        # cv2.imwrite('./my_data/sintra_panorama1234_SIFT.jpg', panorama1234)
+        # plt.figure(6)
+        # plt.imshow(panorama1234)
+        # plt.show()
